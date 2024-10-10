63087774a36db47521c3e4b42d6c52f6e6ae949f
snikuz12 <snikuz12@naver.com>
2024-10-10 10:47:03 +0900

    fix : 오류수정

--- src/main.js
+++ src/main.js
@@ -8,12 +8,10 @@ import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives'; 
 import 'vuetify/styles';
 import '@mdi/font/css/materialdesignicons.css'; 
-import adapter from 'webrtc-adapter';
-import vuetify from './plugins/vuetify';
-import adapter from 'webrtc-adapter';
+import adapter from 'webrtc-adapter'; 
+
 
 window.adapter = adapter;
-
 
 axios.defaults.baseURL = process.env.VUE_APP_API_BASE_URL || 'http://localhost:8087';
 const token = localStorage.getItem('token');
@@ -21,22 +19,17 @@ if (token) {
   axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 }
 
-
-const vuetify = createVuetify({
+const vuetifyInstance = createVuetify({
   components, 
   directives, 
 });
 
-
 const app = createApp(App);
-
 
 app.config.globalProperties.$axios = axios;
 
 app.use(router);
 app.use(store);
-app.use(vuetify);
-
-
+app.use(vuetifyInstance);
 
 app.mount('#app');
