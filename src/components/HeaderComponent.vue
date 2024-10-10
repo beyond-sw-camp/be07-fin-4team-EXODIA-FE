10ce8d6ed7c5d601bb89611a2f5c37ca53904782
suhyunpark <psh7492@gmail.com>
2024-10-10 09:23:44 +0900

    fix: sidebar, header 수정

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,15 +1,14 @@
 <template>
   <header class="header">
-    <div class="logo">
-      <img :src="require('@/assets/exodia.png')" alt="Logo" />
-    </div>
     <div class="icons">
-      <v-icon class="icon">mdi-help-circle-outline</v-icon>
-      <v-icon class="icon">mdi-email</v-icon>
-      <v-icon class="icon">mdi-message</v-icon>
+      <div class="icon-item" @click="$router.push('/calendar/calendarList')"
+        :class="{ 'active': $route.path.startsWith('/calendar') }">
+        <v-icon class="icon">mdi-calendar</v-icon>
+      </div>
       <v-icon class="icon">mdi-bell</v-icon>
-      <v-avatar>
-        <img src="@/assets/user.png" alt="User Avatar" class="user-avatar" />
+      <v-avatar class="icon" @click="$router.push('/mypage/userProfile')">
+        <img src="@/assets/sh.png" alt="User Avatar" class="user-avatar"
+          style="width: 100%; height: 100%; object-fit: cover;" />
       </v-avatar>
     </div>
   </header>
@@ -24,46 +23,52 @@ export default {
 <style scoped>
 .header {
   display: flex;
-  justify-content: space-between;
+  justify-content: end;
   align-items: center;
-  background-color: #e6f1e3;
-  border-bottom: 1px solid #d9e5d6;
   height: 8vh;
   z-index: 1000;
   width: calc(100% - var(--sidebar-width));
   position: fixed;
   left: var(--sidebar-width);
   top: 0;
-}
-
-.logo {
-  display: flex;
-  align-items: center;
-}
-
-.logo img {
-  height: 6vh; 
-  margin-right: 2vw; 
+  font-size: 14px;
 }
 
 .icons {
   display: flex;
   align-items: center;
+  color: #444444;
 }
 
-.icons > .icon {
-  font-size: 4vh; 
-  margin-left: 4vw; 
+.icon {
+  margin-left: 20px;
   cursor: pointer;
+  font-size: 25px;
 }
 
-.icons > v-avatar {
+.icons>v-avatar {
   margin-left: 4vw;
   height: 5vh;
-  width: 5vh; 
+  width: 5vh;
 }
 
+.icon:hover {
+  opacity: 0.5;
+  visibility: visible;
+}
 
+.icon-item.active {
+  color: #7A5656;
+  font-weight: 700;
+  background-color: rgba(122, 86, 86, 0.2);
+  border-radius: 50%;
+  width: 40px;
+  height: 40px;
+  justify-content: center;
+  align-content: center;
+}
+
+.icon-item.active>.icon {
+  color: #7A5656;
+}
 </style>
-
-
