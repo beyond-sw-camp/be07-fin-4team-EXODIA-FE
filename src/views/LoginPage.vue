65d21b85a24f4d80534b2ce5f68eb14765563129
Ki-Wing <dldpsk1541@gmail.com>
2024-10-18 11:47:55 +0900

    feat: 디자인 찐찐막

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -132,67 +132,56 @@ html, body, #app {
 
 .login-container {
   height: 100vh;
-  background: linear-gradient(315deg, rgba(101,0,94,1) 3%, rgba(60,132,206,1) 38%, rgba(48,238,226,1) 68%, rgba(255,25,25,1) 98%);
-  animation: gradient 15s ease infinite;
-  background-size: 400% 400%;
-  background-attachment: fixed;
+  background-color: white;
   position: relative;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.wave-container {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 35vh; /* 높이를 더 올렸습니다 */
   overflow: hidden;
 }
 
-@keyframes gradient {
-  0% {
-    background-position: 0% 0%;
-  }
-  50% {
-    background-position: 100% 100%;
-  }
-  100% {
-    background-position: 0% 0%;
-  }
-}
-
 .wave {
-  background: rgba(255, 255, 255, 0.25);
+  background: rgba(0, 150, 136, 0.25);
   border-radius: 1000% 1000% 0 0;
-  position: fixed;
+  position: absolute;
   width: 200%;
-  height: 12em;
+  height: 15em; /* 높이를 조금 더 키움 */
   animation: wave 10s -3s linear infinite;
   transform: translate3d(0, 0, 0);
   opacity: 0.8;
   bottom: 0;
   left: 0;
-  z-index: -1;
 }
 
 .wave:nth-of-type(2) {
   bottom: -1.25em;
-  animation: wave 18s linear reverse infinite;
+  animation: wave 12s linear reverse infinite;
   opacity: 0.8;
 }
 
 .wave:nth-of-type(3) {
   bottom: -2.5em;
-  animation: wave 20s -1s reverse infinite;
+  animation: wave 17s -1s reverse infinite;
   opacity: 0.9;
 }
 
 @keyframes wave {
-  2% {
-    transform: translateX(1%);
-  }
-  25% {
-    transform: translateX(-25%);
+  0% {
+    transform: translateX(0);
   }
   50% {
     transform: translateX(-50%);
   }
-  75% {
-    transform: translateX(-25%);
-  }
   100% {
-    transform: translateX(1%);
+    transform: translateX(0);
   }
 }
 
