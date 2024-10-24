f70be856e2bdc0e1533d4971eff98b0eabbf1064
leem5514 <ikaros0569@gmail.com>
2024-10-25 06:05:46 +0900

    fix : carReservation

--- src/views/reservation/reservationList.vue
+++ src/views/reservation/reservationList.vue
@@ -47,6 +47,35 @@
       <!-- <v-btn class="ml-2" @click="openLongTermReservationModal" color="primary" outlined>장기 예약</v-btn> -->
     </v-row>
 
+    <v-row justify="space-between" style="margin-left: 1200px">
+      <v-btn icon @click="openFilterModal" style="margin-top: 10px; box-shadow: none">
+        <v-icon>mdi-filter</v-icon> <!-- 필터 아이콘 -->
+      </v-btn>
+    </v-row>
+
+    <!-- 필터 모달창 (당일 예약된 차량만 표시) -->
+    <v-dialog v-model="isFilterModalOpen" max-width="500px">
+      <v-card>
+        <v-card-title>당일 예약된 차량</v-card-title>
+        <v-card-text>
+          <!-- reservedVehicles가 비어 있지 않은지 확인 -->
+          <v-list v-if="reservedVehicles.length">
+            <v-list-item v-for="vehicle in reservedVehicles" :key="vehicle.carId">
+              <v-list-item-content>
+                <v-list-item-title>{{ vehicle.carType }} - {{ vehicle.carNum }}</v-list-item-title>
+                <v-list-item-subtitle>{{ vehicle.userName }}님 예약 중</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+          <div v-else>당일 예약된 차량이 없습니다.</div>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" text @click="isFilterModalOpen = false">닫기</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
 
     <!-- 현재 탭에 대한 내용 -->
     <v-tabs-items v-model="selectedTab">
@@ -70,11 +99,13 @@
 
             <!-- 차량 리스트 -->
             <v-row v-for="vehicle in vehicles" :key="vehicle.carId" class="vehicle-row" outlined style="border-bottom:1px solid #E7E4E4; padding:5px; font-weight:300;">
+              
               <v-col cols="1">
                 <v-btn @click="openCarModal(vehicle)" icon color="none" style="box-shadow: none;">
-                  <v-icon>mdi-car</v-icon> <!-- 차량 아이콘 -->
+                  <v-icon>mdi-car</v-icon>
                 </v-btn>
               </v-col>
+ 
               
               <!-- 차량 종류 -->
               <v-col cols="3" class="d-flex align-center justify-center">{{ vehicle.carType }}</v-col>
@@ -126,6 +157,7 @@
               height="200px"
               max-height="200px"
               contain
+              style="margin-top: 10px;"
               class="d-flex align-center justify-center"
             ></v-img>
 
@@ -137,10 +169,10 @@
               <v-row align="center" justify="space-between">
                 <v-col class="d-flex align-center">
                   <v-icon left>mdi-seat</v-icon>
-                  <span>{{ selectedCar.seatingCapacity }} 인승</span>
+                  <span>{{ selectedCar.carSeat }} 인승</span>
                 </v-col>
                 <v-col class="d-flex align-center">
-                  <span>배기량: {{ selectedCar.engineDisplacement }}L</span>
+                  <span>배기량: {{ selectedCar.carEngine }}L</span>
                 </v-col>
               </v-row>
             </v-card-text>
@@ -165,7 +197,8 @@
                 <!-- 차량 선택 -->
                 <v-select
                   v-model="selectedVehicle"
-                  :items="vehicles.map(vehicle => ({ title: vehicle.carNum, value: vehicle.carId }))"
+                  
+                  :items="vehicles.map(vehicle => ({ title: `${vehicle.carType} (${vehicle.carNum})`, value: vehicle.carId }))"
                   label="차량 선택"
                   required
                 ></v-select>
@@ -176,7 +209,7 @@
                     <v-text-field
                       v-model="startDate"
                       label="시작 날짜"
-                      
+                      type="date" 
                       v-bind="attrs"
                       v-on="on"
                     ></v-text-field>
@@ -190,7 +223,7 @@
                     <v-text-field
                       v-model="endDate"
                       label="끝 날짜"
-                      
+                      type="date"
                       v-bind="attrs"
                       v-on="on"
                     ></v-text-field>
@@ -233,14 +266,23 @@ export default {
       selectedVehicle: null, // 선택된 차량
       menuStart: false,
       menuEnd: false,
+      isFilterModalOpen: false, // 필터 모달 상태
     };
   },
   computed: {
     ...mapGetters({
       departmentName: "getDepartmentName",
     }),
+    reservedVehicles() {
+      return this.vehicles.filter(vehicle => vehicle.status === 'RESERVED');
+    },
   },
   methods: {
+    openFilterModal() {
+      console.log("reservedVehicles:", this.reservedVehicles); // reservedVehicles 데이터 출력
+      this.isFilterModalOpen = true;
+    },
+
     ...mapActions(["setUserAllInfoActions"]), // 사용자 정보를 Vuex에 저장하는 액션 호출
     
     formattedDate(date) {
