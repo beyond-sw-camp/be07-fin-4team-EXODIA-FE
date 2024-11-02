a5110bf0a025eabd60c27b64b99d5be3474e4ea3
Ki-Wing <dldpsk1541@gmail.com>
2024-11-03 02:17:05 +0900

    test

--- src/views/video/VideoRoom.vue
+++ src/views/video/VideoRoom.vue
@@ -2,11 +2,11 @@
   <div class="room-view">
     <h2>화상회의 방: {{ roomTitle }}</h2>
     <div class="main-video">
-      <video ref="mainVideo" autoplay></video>
+      <video ref="mainVideo" autoplay playsinline></video>
     </div>
     <div class="side-videos">
       <div v-for="(video, index) in sideVideos" :key="index" class="side-video" @click="switchToMain(video)">
-        <video :srcObject="video.stream" autoplay muted></video>
+        <video :srcObject="video.stream.getMediaStream()" autoplay playsinline muted></video>
       </div>
     </div>
     <button @click="leaveRoom">방 나가기</button>
@@ -29,6 +29,7 @@ export default {
     this.initializeRoom();
   },
   methods: {
+
     async initializeRoom() {
   const { sessionId } = this.$route.params;
   try {
@@ -49,13 +50,21 @@ export default {
     await this.session.connect(token, { clientData: "사용자명" });
 
     this.publisher = this.OV.initPublisher(undefined, {
-      videoSource: undefined,
-      publishAudio: true,
-      publishVideo: true,
+      videoSource: undefined, // 디폴트 카메라 사용
+      audioSource: undefined, // 디폴트 마이크 사용
+      publishAudio: true,     // 오디오 켜기
+      publishVideo: true,     // 비디오 켜기
+      resolution: '640x480',  // 해상도 설정
+      frameRate: 30,          // 프레임 설정
+      insertMode: 'APPEND',   // 비디오 추가 모드
+      mirror: false           // 미러링 비활성화 (필요에 따라 설정)
     });
-    this.mainVideo.srcObject = this.publisher.stream.getMediaStream();
-    this.session.publish(this.publisher);
 
+    this.publisher.once('accessAllowed', () => {
+      this.mainVideo.srcObject = this.publisher.stream.getMediaStream();
+    });
+
+    this.session.publish(this.publisher);
   } catch (error) {
     console.error("방 참여 중 오류 발생:", error);
   }
