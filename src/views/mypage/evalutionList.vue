adc3773d3c23090632da963939398d03f5fd42f0
suhyunpark <psh7492@gmail.com>
2024-11-04 12:04:40 +0900

    fix: 직급별 평가 대상 변경

--- src/views/mypage/evalutionList.vue
+++ src/views/mypage/evalutionList.vue
@@ -1,51 +1,107 @@
 <template>
   <MypageTemplate>
     <template #evaluation>
-      <v-row class="mt-4">
-        <v-col v-if="isManager" cols="12" md="4">
+      <v-row style="margin-top: 10px;">
+
+        <!-- 팀원은 팀장이 -->
+        <!-- 팀장은 부서장, 본부장 -->
+        <!-- 부서장, 본부장은 본인이 -->
+        <v-col v-if="this.positionId >= 5 || this.positionId <= 7" cols="12" md="4">
           <v-select v-model="selectedUser" :items="departmentUsers" item-title="name" item-value="userNum"
             label="부서원 선택" dense outlined></v-select>
         </v-col>
       </v-row>
 
-      <v-row>
-        <v-col>
-          <v-simple-table dense>
-            <thead>
-              <tr style="background-color:rgba(122,86,86,0.2);">
-                <th style="padding:10px;  border: 1px solid #f5f5f5">대분류</th>
-                <th style="padding:10px;  border: 1px solid #f5f5f5">중분류</th>
-                <th style="padding:10px;  border: 1px solid #f5f5f5">평가 내용</th>
-                <th style="padding:10px;  border: 1px solid #f5f5f5">평가</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="(item, index) in evaluations" :key="index">
-                <td v-if="shouldShowBigCategory(index)" :rowspan="getRowspanForBigCategory(index)"
-                  style="width: 200px; text-align: center; font-weight: bold; border: 1px solid #e0e0e0;">
-                  {{ item.bigCategoryName }}
-                </td>
-                <td style="width: 200px; text-align: center; border: 1px solid #e0e0e0;">
-                  {{ item.midCategoryName }}
-                </td>
-                <td style="border: 1px solid #e0e0e0; text-align: center; width: 800px;">
-                  {{ item.subEvalutionContent }}
-                </td>
-                <td style="border: 1px solid #e0e0e0; text-align: center;">
-                  <!-- 평가 선택 -->
-                  <v-select hide-details v-model="item.grade" :items="['A', 'B', 'C', 'D', 'E']" label="평가 선택" dense
-                    style="width: 100px; background-color: #FFFFFF; border: none"
-                    :disabled="item.saved && !item.editable"></v-select>
-                </td>
-              </tr>
-            </tbody>
-          </v-simple-table>
-        </v-col>
-      </v-row>
+      <!-- 인사평가 기간이라면 평가할 수 있도록 -->
+      <div v-if="isEvaluationPeriod" style="padding:16px; margin-top:10px;">
+        <v-row>
+          <v-col>
+            <v-table dense>
+              <thead>
+                <tr style="background-color:rgba(122,86,86,0.2);font-weight:800">
+                  <th style="padding:10px;  border: 1px solid #f5f5f5; text-align: center;font-weight:800">대분류</th>
+                  <th style="padding:10px;  border: 1px solid #f5f5f5; text-align: center; font-weight:800">중분류</th>
+                  <th style="padding:10px; text-align: center; font-weight:800">소분류</th>
+                  <th style="padding:10px;  border: 1px solid #f5f5f5; text-align: center; font-weight:800">평가</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="(item, index) in evaluations" :key="index">
+                  <td v-if="shouldShowBigCategory(index)" :rowspan="getRowspanForBigCategory(index)"
+                    style="width: 200px; text-align: center; font-weight: bold; border: 1px solid #e0e0e0;">
+                    {{ item.bigCategoryName }}
+                  </td>
+                  <td style="width: 200px; text-align: center; border: 1px solid #e0e0e0;">
+                    {{ item.midCategoryName }}
+                  </td>
+                  <td style="border: 1px solid #e0e0e0; text-align: center; width: 800px;">
+                    {{ item.subEvalutionContent }}
+                  </td>
+                  <td style="border: 1px solid #e0e0e0; text-align: center; padding:0 ">
+                    <!-- 평가 선택 -->
+                    <v-select hide-details v-model="item.grade" :items="['A', 'B', 'C', 'D', 'E']" label="평가 선택" dense
+                      style="width: 100px; background-color: #FFFFFF; border: none; padding:0"
+                      :disabled="item.saved && !item.editable"></v-select>
+                  </td>
+                </tr>
+              </tbody>
+            </v-table>
+          </v-col>
+        </v-row>
 
-      <!-- 저장 버튼 -->
-      <v-row justify="end">
-        <v-btn v-create @click="saveEvaluations">저장</v-btn>
+        <!-- 저장 버튼 -->
+        <v-row justify="end">
+          <v-btn v-create @click="saveEvaluations">저장</v-btn>
+        </v-row>
+      </div>
+
+      <!-- 인사평가 기간이 아니라면 -->
+      <div v-if="!isEvaluationPeriod" style="padding:16px; margin-top:10px;">
+        <v-row>
+          <v-col>
+            <v-table dense>
+              <thead>
+                <tr style="background-color:rgba(122,86,86,0.2);">
+                  <th style="padding:10px;  border: 1px solid #f5f5f5">대분류</th>
+                  <th style="padding:10px;  border: 1px solid #f5f5f5">중분류</th>
+                  <th style="padding:10px;  border: 1px solid #f5f5f5">평가 내용</th>
+                  <th style="padding:10px;  border: 1px solid #f5f5f5">평가</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="(item, index) in avgEvalutions" :key="index">
+                  <td v-if="shouldAvgShowBigCategory(index)" :rowspan="getAvgRowspanForBigCategory(index)"
+                    style="width: 200px; text-align: center; font-weight: bold; border: 1px solid #e0e0e0;">
+                    {{ item.bigCategoryName }}
+                  </td>
+                  <td style="width: 200px; text-align: center; border: 1px solid #e0e0e0;">
+                    {{ item.midCategoryName }}
+                  </td>
+                  <td style="border: 1px solid #e0e0e0; text-align: center; width: 800px;">
+                    {{ item.subEvalutionContent }}
+                  </td>
+                  <td
+                    style="border: 1px solid #e0e0e0; text-align: center; width: 100px;height:56px; font-weight: 800; ">
+                    {{ item.avgScore }}
+                  </td>
+                </tr>
+
+                <tr style="height:56px; border:none; font-weight: 800;">
+                  <td colspan="3" style="text-align:right; border:none; padding-right: 10px;">
+                    총점:
+                  </td>
+                  <td style="text-align: center; border:none;">
+                    {{ totalScore }}
+                  </td>
+                </tr>
+              </tbody>
+            </v-table>
+          </v-col>
+        </v-row>
+      </div>
+
+      <v-row v-if="isEvaluationPeriod && isEvaluated">
+        이미 평가한 기록이 존재합니다.
       </v-row>
     </template>
   </MypageTemplate>
@@ -61,15 +117,32 @@ export default {
   },
   data() {
     return {
+      positionId: localStorage.getItem('positionId'),
+
       evaluations: [],
       departmentUsers: [],
       selectedUser: null,
       isManager: false,
+
+      isEvaluated: false,
+      avgEvalutions: [],
+      totalScore: '',
     };
   },
   created() {
     this.checkIfManager();
-    this.fetchSubEvalutions();
+    if (this.isEvaluationPeriod) {
+      this.fetchSubEvalutions();
+
+    } else {
+      this.fetchAvgEvalutaions();
+    }
+
+  },
+  computed: {
+    isEvaluationPeriod() {
+      return this.$route.query.isEvaluationPeriod === 'true';
+    }
   },
   watch: {
     selectedUser(newValue) {
@@ -81,20 +154,29 @@ export default {
   methods: {
     async checkIfManager() {
       try {
-        const positionId = localStorage.getItem('positionId');
+        const departmentId = localStorage.getItem('departmentId');
 
-        if (positionId === '7') {
-          this.isManager = true;
-          const departmentId = localStorage.getItem('departmentId');
-          const departmentResponse = await axios.get(`${process.env.VUE_APP_API_BASE_URL}/user/department-users/${departmentId}`, {
-            headers: {
-              Authorization: `Bearer ${localStorage.getItem('token')}`
-            }
-          });
-          this.departmentUsers = departmentResponse.data;
-        } else {
-          this.isManager = false;
+        let url = '';
+
+        if (this.positionId == 7) { //팀장
+          url = `${process.env.VUE_APP_API_BASE_URL}/user/department-users/${departmentId}`;
+        } else if (this.positionId == 6 || this.positionId == 5) {
+          url = `${process.env.VUE_APP_API_BASE_URL}/attendance/department/list`;
         }
+
+        const response = await axios.get(url, {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('token')}`
+          }
+        });
+        this.departmentUsers = response.data.result;
+
+        if (this.positionId == 6 || this.positionId == 5) {
+          this.departmentUsers = response.data.result.filter(user => user.positionId <= 7);
+        }
+        // else {
+        //   this.departmentUsers = response.data.result;
+        // }
       } catch (error) {
         console.error('팀장 여부 확인 실패 또는 부서원 목록 조회 실패', error);
       }
@@ -113,9 +195,9 @@ export default {
         this.evaluations = response.data.map(item => ({
           ...item,
           subEvalutionContent: item.subEvalutionContent || '',
-          grade: item.grade || '',
-          saved: !!item.grade,
-          editable: !item.grade
+          score: item.score || '',
+          saved: !!item.score,
+          editable: !item.score,
         }));
       } catch (error) {
         console.error('평가 목록 조회 실패', error);
@@ -160,7 +242,39 @@ export default {
         }
       }
       return rowspan;
-    }
+    },
+    shouldAvgShowBigCategory(index) {
+      if (index === 0) return true;
+      return this.avgEvalutions[index].bigCategoryName !== this.avgEvalutions[index - 1].bigCategoryName;
+    },
+    getAvgRowspanForBigCategory(index) {
+      const currentCategory = this.avgEvalutions[index].bigCategoryName;
+      let rowspan = 0;
+      for (let i = index; i < this.avgEvalutions.length; i++) {
+        if (this.avgEvalutions[i].bigCategoryName === currentCategory) {
+          rowspan++;
+        } else {
+          break;
+        }
+      }
+      return rowspan;
+    },
+    async fetchAvgEvalutaions() {
+      try {
+        const response = await axios.post(`${process.env.VUE_APP_API_BASE_URL}/evalution-avg/calculate`, {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('token')}`,
+          },
+        });
+        this.avgEvalutions = response.data.result;
+
+        this.totalScore = this.avgEvalutions.reduce((sum, item) => sum + item.avgScore, 0);
+        this.totalScore /= 7;
+
+      } catch (error) {
+        console.error('소분류 불러오기 실패', error);
+      }
+    },
   }
 };
 </script>
